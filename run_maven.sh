199572e2c5fcb8c93fb7f233e95f62f0631d9cf9
matheus-andrew <68177071+matheus-andrew@users.noreply.github.com>
2024-08-02 23:08:56 +0700

    Update typo format

--- run_maven.sh
+++ run_maven.sh
@@ -14,20 +14,20 @@ COLOR_OFF='\033[0m'
 
 # Run tests multiple times with different report filenames
 for i in $(seq 1 $1); do
-    echo -e "${CYAN}Start running iteration $i...%{COLOR_OFF}"
+    echo -e "${CYAN}Start running iteration $i...${COLOR_OFF}"
 
     # Run tests and generate report
     mvn clean install
-    echo -e "${CYAN}Maven runner completed.%{COLOR_OFF}"
+    echo -e "${CYAN}Maven runner completed.${COLOR_OFF}"
 
     # Copy the report and rename it
     cp target/cucumber.html "$REPORT_DIR/cucumber_$i.html"
-    echo -e "${CYAN}Success copied HTML report.%{COLOR_OFF}"
+    echo -e "${CYAN}Success copied HTML report.${COLOR_OFF}"
 
     # Optional: Pause between runs if needed
     sleep 5
 
     # Kill all Chrome processes
     pkill -f chrome
-    echo -e "${CYAN}All Chrome processes terminated.%{COLOR_OFF}"
+    echo -e "${CYAN}All Chrome processes terminated.${COLOR_OFF}"
 done
